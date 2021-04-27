6afac0deccd81b2689779da8e4db0de47cf91294
ryrua14 <ryru136@gmail.com>
2021-04-27 14:46:13 -0400

    Project reads text file

--- scripts/read_text.js
+++ scripts/read_text.js
@@ -1,10 +1,17 @@
+var inputText = document.querySelector('.text');
+
 document.getElementById('file-selector').addEventListener('change', (event)=>{
-    const file = event.target.files;
+    var file = event.target.files[0];
     console.log(file);
-   const reader = new FileReader();
+    const reader = new FileReader();
     reader.addEventListener('load',(event)=>{
-        inputText.src = event.target.result;
+        inputText.value = event.target.result;
+        console.log(inputText);
     });
+    //Get result from text file and have synthesizer read the text file
     reader.readAsText(file);
     console.log(reader);
+    inputText.value = reader.result;
+    inputText.innerHTML = inputText.value;
+    console.log(inputText);
 });
